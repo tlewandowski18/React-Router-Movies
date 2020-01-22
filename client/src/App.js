3bd645fa02a8d59d0def04488608ea13f8b9dd54
tlewandowski18 <tlewandowski18@gmail.com>
2020-01-21 19:51:10 -0500

    Add Routes to Movie and MovieList

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 import SavedList from './Movies/SavedList';
+import { Route } from 'react-router-dom'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -9,10 +11,12 @@ const App = () => {
     setSavedList( [...savedList, movie] );
   };
 
+  console.log(savedList);
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie} />
     </div>
   );
 };
